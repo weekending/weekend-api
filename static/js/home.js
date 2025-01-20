ffcdb5c4ce18b4b84696911268e7a0f2c3fa9b35
miintto <miintto.log@gmail.com>
2025-01-21 00:45:24 +0900

    feat: 일정관리 탭 추가

--- static/js/home.js
+++ static/js/home.js
@@ -16,3 +16,24 @@ const switchTab = (event) => {
 document.querySelectorAll('.nav-item').forEach(element => {
   element.addEventListener('click', (e) => switchTab(e))
 });
+
+const circleButtonAction = () => {
+  const ids = ['button-register', 'button-pick', 'button-schedule'];
+  if (document.getElementById('button-plus').getAttribute('open')) {
+    document.getElementById('screen').setAttribute('hidden', true);
+    for (let i = 0; i < ids.length; i++) {
+      element = document.getElementById(ids[i]);
+      element.style.bottom = 0;
+      element.style.opacity = 0;
+    }
+    document.getElementById('button-plus').removeAttribute('open');
+  } else {
+    document.getElementById('screen').removeAttribute('hidden');
+    for (let i = 0; i < ids.length; i++) {
+      element = document.getElementById(ids[i]);
+      element.style.bottom = 58 * (i + 1) + 'px';
+      element.style.opacity = 1;
+    }
+    document.getElementById('button-plus').setAttribute('open', true);
+  }
+}
